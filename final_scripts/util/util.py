30a580d5c6c88befd622ecf9de93bb4f4cbe662b
Takanori Watanabe (PC) <takwatanabe2004@gmail.com>
2017-01-12 08:34:25 -0500

    ...intermediate update...working on network theory...

--- final_scripts/util/util.py
+++ final_scripts/util/util.py
@@ -2,13 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn.apionly as sns
-import os
 import time
-import sys
+import bct
 import networkx as nx
 
-from datetime import datetime
-from pprint import pprint
 #%% === set plotting options ====
 def sns_figure(style='darkgrid',**kwargs):
     """ Create new figure in seaborn with temporary style change
@@ -151,6 +148,113 @@ def make_hash_tables(df_lookup):
             hash_tables[col] = df_lookup.set_index('Code')[col].to_dict()
 
     return hash_tables
+#%% === network analysis routine ===
+def make_nx_graph(counts,df_lookup,digraph=False):
+    """
+    
+    Parameters
+    ----------
+    counts : pandas.DataFrame
+        Table containing the trip_counts (digraph) or route_counts (undirected graph)
+        Use in network analysis scripts
+    df_lookup : pandas.DataFrame
+        Table 
+    digraph : bool
+        Is the graph directed? (default = False, so undirected)
+    """
+    if digraph:
+        G = nx.DiGraph() # directed graph
+    else: 
+        G = nx.Graph()   # undirected graph
+
+    # === provide node information === #
+    # get unique set of nodes in the graph
+    nodes = set(counts['code1'].unique().tolist() +
+                counts['code2'].unique().tolist())
+                
+    G.add_nodes_from(nodes)
+    
+    # --- add airport name as node attribute (handy for analysis in Gephi) ---
+    # --- to do this, need to pass a dictionary to networkx 
+    hash_airport   = df_lookup.set_index('Code')['Airport'].to_dict()
+
+    # filter away airport in the lookup-table not in the graph
+    nodes_airport = {key:val for key,val in hash_airport.iteritems() if key in nodes}
+    nx.set_node_attributes(G, 'airport', nodes_airport)
+    
+    # --- add airport latitude/longitude information --- 
+    hash_lat       = df_lookup.set_index('Code')['lat'].to_dict()
+    hash_lon       = df_lookup.set_index('Code')['lon'].to_dict()
+    
+    """ Warning (why the typecasting below is important)
+    Pandas returns data type in numpy.float64 for floats, which is not 
+    supported in ``networkx.write_gexf`` (learned this the hard way...)
+    
+    http://stackoverflow.com/questions/22037360/keyerror-when-writing-numpy-values-to-gexf-with-networkx
+    """
+    # apply filering with typecasting from numpy.float64 to float
+    nodes_lat = {key:float(val) for key,val in hash_lat.iteritems() if key in nodes}
+    nodes_lon = {key:float(val) for key,val in hash_lon.iteritems() if key in nodes}
+    nx.set_node_attributes(G, 'lat', nodes_lat)
+    nx.set_node_attributes(G, 'lon', nodes_lon)
+    
+
+    # === add weighted edge information (flight counts in our context) === #
+    # to define edge, supply a 3-tuple of ``(node1,node2,dict(weight=edge))``
+    edges = map(lambda x:(x[0],x[1], dict(weight=x[2])), 
+                zip(counts['code1'], counts['code2'], counts['counts']))
+    G.add_edges_from(edges)
+    
+    # === done! ready to return, except one more step! === 
+    # instead of using the Airport_ID as the node-labels, let's instead use the
+    # City+State information, which is unique so can be used as lookup-keys
+    hash_citystate = df_lookup.set_index('Code')['City_State'].to_dict()
+    nodes_citystate = {key:val for key,val in hash_citystate.iteritems() if key in nodes}
+    G = nx.relabel_nodes(G,nodes_citystate)
+    return G
+    
+def compute_network_measures(G,add_module_attr = True):
+    
+    A = np.array(nx.to_numpy_matrix(G))
+    
+    degree_wei = A.sum(axis=0,dtype=int) # weighted degree 
+    degree_bin = (A!=0).sum(axis=0)      # binary degree
+    
+    # community detection -> compute modularity groups
+    module = bct.modularity_louvain_und(bct.normalize(A),seed=0)[0]
+    
+    # convert numpy array into dictionary with node-label
+    degree_wei = {node:deg for node,deg in zip(G.nodes(), degree_wei)}
+    degree_bin = {node:deg for node,deg in zip(G.nodes(), degree_bin)}
+    module = {node:int(modu) for node,modu in zip(G.nodes(), module)}
+    
+    if add_module_attr:
+        # add community label as node attributes 
+        # (handy when exporting .gexf file)
+        nx.set_node_attributes(G, 'modu', module)
+    
+    # nodal centrality measures
+    pagerank = nx.pagerank(G)               # google page-rank
+    eig_cent = nx.eigenvector_centrality(G) # eigenvalue centrality
+    bet_cent = nx.betweenness_centrality(G) # betweennes centrality
+    
+    # create dictionary of each of the measures computed above
+    # (to be convertd to dataframe at end)
+    df_network = dict(pagerank=pagerank,
+                      eig_cent=eig_cent,
+                      bet_cent=bet_cent,
+                      degree_wei=degree_wei,
+                      degree_bin=degree_bin,
+                      module=module)
+                      
+    if not isinstance(G, nx.classes.digraph.DiGraph):
+        # clustering coefficient (tendency of a node to cluster together)
+        # (not implemented for digraphs)
+        clust_coef = nx.clustering(G,weight=G)
+        df_network.update(dict(clust_coef=clust_coef))
+    
+    # all set! convert dict to dataframe and return :)
+    return pd.DataFrame(df_network)
 #%% === other utility functions ===
 def print_time(t):
     """ Return string indicating times elapsed wrt a time.time() instance
